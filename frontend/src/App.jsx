3197268a5e61580c4d02ce7cf7fe6258c0c31c3f
ameer017 <rajiabdullahi907@gmail.com>
2024-03-14 16:20:30 +0100

    complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import "./App.css";
 import HomeDash from "./Component/Dashboard/HomeDash";
 import StudentReg from "./Component/Register/StudentReg";
 import Layout from "./Component/Layout/Layout";
+import Loader from "./Component/Loader/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
-  return (
-    <>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Layout>
-              <HomeDash />
-            </Layout>
-          }
-        />
-        <Route
-          path="/student-reg"
-          element={
-            <Layout>
-              <StudentReg />
-            </Layout>
-          }
-        />
-      </Routes>
-    </>
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  const renderRoutes = () => (
+    <Routes>
+      <Route path="/" element={<StudentReg />} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomeDash />
+          </Layout>
+        }
+      />
+    </Routes>
   );
+
+  return <>{loading ? <Loader /> : renderRoutes()}</>;
 }
 
 export default App;
--- frontend/src/Component/Loader/Loader.jsx
+++ frontend/src/Component/Loader/Loader.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import './Loader.css'
+import React from "react";
+import "./Loader.css";
 
 const Loader = () => {
   return (
-    <div id='loader'></div>
-  )
-}
+    <div className="--100vh --flex-center">
+      <div id="loader"></div>
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
