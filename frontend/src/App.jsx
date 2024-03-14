3676232476cf9f42098ae53126848d44fadec909
ameer017 <rajiabdullahi907@gmail.com>
2024-03-14 15:22:04 +0100

    more_config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import HomeDash from "./Component/Dashboard/HomeDash";
@@ -8,7 +6,6 @@ import StudentReg from "./Component/Register/StudentReg";
 import Layout from "./Component/Layout/Layout";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
