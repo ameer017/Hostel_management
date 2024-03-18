13035daa77ff0e523372c0db89dba858848f1a95
ameer017 <rajiabdullahi907@gmail.com>
2024-03-18 12:12:59 +0100

    table updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import StudentReg from "./Component/Register/StudentReg";
 import Layout from "./Component/Layout/Layout";
 import Loader from "./Component/Loader/Loader";
 import StudentDashboard from "./Component/Dashboard/StudentDashboard";
-import Attendance from "./Component/Attendance/Attendance";
 
 function App() {
   const [loading, setLoading] = useState(true);
