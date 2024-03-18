d017316a9854cac2d3f36c3dc4e1709da195cba2
Anaboy01 <aliyuanate016@gmail.com>
2024-03-18 16:15:38 +0100

    cool commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
       />
 
 
-      <Route path="/studentdash" element={<StudentDashboard />} />
+      {/* <Route path="/studentdash" element={<StudentDashboard />} /> */}
 
        <Route path="/adminsPrev" element={<AdminPreview />} />
 
