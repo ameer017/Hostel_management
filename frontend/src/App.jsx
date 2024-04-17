b89f1898a0d7bdec8f36819d56abe8b729d6285a
ameer017 <rajiabdullahi907@gmail.com>
2024-04-17 14:49:40 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import HomeDash from "./Component/Dashboard/HomeDash";
 import StudentReg from "./Component/Register/StudentReg";
 import AdminReg from "./Component/Register/AdminReg";
 import Layout from "./Component/Layout/Layout";
-import Loader from "./Component/Loader/Loader";
 
 import AdminPreview from "./Component/AdminPreview/AdminPreview";
 import StudentDashboard from "./Component/Dashboard/StudentDashboard";
@@ -13,15 +12,7 @@ import Login from "./Component/Register/Login";
 import Room from "./Component/Dashboard/Room";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
-
-  const renderRoutes = () => (
+  return (
     <Routes>
       <Route path="/" element={<AdminReg />} />
       <Route path="/login" element={<Login />} />
@@ -41,8 +32,6 @@ function App() {
       <Route path="/adminsPrev" element={<AdminPreview />} />
     </Routes>
   );
-
-  return <>{loading ? <Loader /> : renderRoutes()}</>;
 }
 
 export default App;
--- frontend/src/Component/Register/AdminReg.jsx
+++ frontend/src/Component/Register/AdminReg.jsx
@@ -1,59 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Register.css";
+import Loader from "../Loader/Loader";
+import { Link } from "react-router-dom";
 
 const AdminReg = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <div className="container form__ --100vh">
-      <div className="form-container">
-        <p className="title">Create an account</p>
-        <form className="form">
-          <div className="--dir-column">
-            <label htmlFor="name">Full name:</label>
-            <input
-              type="text"
-              className="input"
-              name="name"
-              placeholder="Enter your full name"
-              required
-            />
-          </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="container form__ --100vh">
+          <div className="form-container">
+            <p className="title">Create an account</p>
+            <form className="form">
+              <div className="--dir-column">
+                <label htmlFor="name">Full name:</label>
+                <input
+                  type="text"
+                  className="input"
+                  name="name"
+                  placeholder="Enter your full name"
+                  required
+                />
+              </div>
 
-          <div className="--dir-column">
-            <label htmlFor="name">Email:</label>
-            <input
-              type="email"
-              className="input"
-              name="email"
-              placeholder="Enter your email"
-              required
-            />
-          </div>
+              <div className="--dir-column">
+                <label htmlFor="name">Email:</label>
+                <input
+                  type="email"
+                  className="input"
+                  name="email"
+                  placeholder="Enter your email"
+                  required
+                />
+              </div>
 
-          <div className="--dir-column">
-            <label htmlFor="name">Password:</label>
-            <input
-              type="password"
-              className="input"
-              name="password"
-              placeholder="Enter your password"
-              required
-            />
-          </div>
+              <div className="--dir-column">
+                <label htmlFor="name">Password:</label>
+                <input
+                  type="password"
+                  className="input"
+                  name="password"
+                  placeholder="Enter your password"
+                  required
+                />
+              </div>
 
-          <div className="--dir-column">
-            <label htmlFor="name">Confirm password:</label>
-            <input
-              type="password"
-              className="input"
-              name="password"
-              placeholder="Re-enter your password"
-              required
-            />
+              <div className="--dir-column">
+                <label htmlFor="name">Confirm password:</label>
+                <input
+                  type="password"
+                  className="input"
+                  name="password"
+                  placeholder="Re-enter your password"
+                  required
+                />
+              </div>
+              <button className="--btn">Create account</button>
+            </form>
+            <p>
+              Already have an account? <Link to="/login">Login</Link>
+            </p>
           </div>
-          <button className="--btn">Create account</button>
-        </form>
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 
