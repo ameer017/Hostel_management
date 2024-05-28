d2798e593d1e747957d3b5d4413df243b8e4c969
ameer017 <rajiabdullahi907@gmail.com>
2024-05-28 21:45:47 +0100

    final-check

--- backend/models/studentModel.js
+++ backend/models/studentModel.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const guardianSchema = new mongoose.Schema({
   guardianName: {
@@ -7,88 +7,84 @@ const guardianSchema = new mongoose.Schema({
   },
   guardianEmail: {
     type: String,
-    required: [true, 'Please add an email'],
+    required: [true, "Please add an email"],
     trim: true,
-    
+  },
+});
+
+const studentSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["Female", "Male"],
+    },
+    nationality: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      trim: true,
+      unique: true,
+    },
+    guardian: guardianSchema,
+    room: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Room",
+      default: null,
+    },
+    role: {
+      type: String,
+      enum: ["student"],
+      default: "student",
+    },
+    checkedIn: {
+      type: Boolean,
+      default: false,
+    },
+    checkInTime: {
+      type: Date,
+      default: null,
+    },
+    checkOutTime: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+    minimize: false,
+    toJSON: { getters: true },
   }
-});
+);
 
-const studentSchema = new mongoose.Schema({
-  _id: {
-    type: String,
-    required: true,
-    unique: true 
-  },
-  firstname: {
-    type: String,
-    required: true,
-  },
-  lastname: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  gender: {
-    type: String,
-    required: true,
-    enum: ['female', 'male']
-  },
-  nationality: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: [true, 'Please add an email'],
-    trim: true,
-    unique: true,
-    
-  },
-  guardian: guardianSchema,
-  room: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Room',
-    default: null,
-  },
-  role: {
-    type: String,
-    enum: ['student'],
-    default: 'student',
-  },
-  checkedIn: {
-    type: Boolean,
-    default: false,
-  },
-  checkInTime: {
-    type: Date,
-    default: null,
-  },
-  checkOutTime: {
-    type: Date,
-    default: null,
-  },
-}, {
-  timestamps: true,
-  minimize: false,
-  toJSON: { getters: true },
-});
-
-studentSchema.methods.checkIn = function() {
+studentSchema.methods.checkIn = function () {
   this.checkedIn = true;
   this.checkInTime = new Date();
   this.checkOutTime = null;
 };
 
-studentSchema.methods.checkOut = function() {
+studentSchema.methods.checkOut = function () {
   this.checkedIn = false;
   this.checkOutTime = new Date();
-  this.checkInTime = null
- 
+  this.checkInTime = null;
 };
 
-const Student = mongoose.model('Student', studentSchema);
+const Student = mongoose.model("Student", studentSchema);
 
 module.exports = Student;
